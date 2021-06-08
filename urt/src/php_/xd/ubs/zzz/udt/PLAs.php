5d8021f7e86613c0b94dc62cf7360650ce9c3b25
Rbgo Web <rbgo.web@gmail.com>
2021-06-08 18:13:54 +0200

    ADD *PLAs*
    Represents the definition of Programming Language Abbreviations.
    The initial set is what's UBS runs on, while at a later stage,
    other PLAs may arrive that find benefit in the Unisrc code
    accommodating scheme.
    
    2 files:
    + urt/src/php_/xd/ubs/zzz/udt/PLAs.php
    Read interface to the definition table.
    
    + urt/ast/www/xd/ubs/zzz/udt/PLAs.jsun
    The definition table.

---
+++ urt/src/php_/xd/ubs/zzz/udt/PLAs.php
@@ -0,0 +1,45 @@
+<?php
+namespace Unisrc\xd\ubs\zzz\udt;
+
+use Unisrc\xu\lib\env;
+
+use Unisrc\xu\lib\pst\enc\jsunLoad;
+use Unisrc\xu\lib\arr\zb\tbl\values;
+
+/*
+DESCR:
+	PLA means Programming Language Abbreviation / PLatform.
+
+	The initial PLAs in Unisrc are:
+	"js_", "jsb", "jsc", "php_", "phpc" and "phph"
+	which covers the PLAs Unisrc is implemented in.
+
+	PLAs are used as directory components in "unisrc/urt/src/*".
+	e.g.:
+		"unisrc/src/jsb" for browser DOM JavaScript code
+		"unisrc/src/phph" for HTTPd PHP code
+
+	...to distinguish source code on language and within that on other
+	targets and purposes.
+
+*/
+class PLAs {
+	
+	public static function keys(){
+
+		static $keys;
+
+		if($keys) return $keys;
+
+		return $keys = values::_(self::table(), 0);
+	}
+
+	public static function table(){
+
+		static $table;
+
+		if($table) return $table;
+
+		return $table = jsunLoad::_(env::_('astwww').'xd/ubs/zzz/udt/PLAs.jsun');
+	}
+}
