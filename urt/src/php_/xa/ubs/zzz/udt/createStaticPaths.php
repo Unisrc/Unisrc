881522ab1b2fd15080cf9e2bd01725adfdb23bd4
Rbgo Web <rbgo.web@gmail.com>
2021-06-17 14:58:12 +0200

    UPD .../xa/ubs/zzz/udt/createStaticPaths.php
    + update namespace
    + add own list func
    + arrange own feedback with class Fb.

--- urt/src/php_/xa/ubs/zzz/udt/createStaticPaths.php
+++ urt/src/php_/xa/ubs/zzz/udt/createStaticPaths.php
@@ -1,21 +1,48 @@
 <?php
-namespace Unisrc\xd\ubs\zzz\udt\git;
+namespace Unisrc\xa\ubs\zzz\udt;
 
 use Unisrc\xu\lib\env;
+use Unisrc\xu\lib\txt\Fb;
 use Unisrc\xu\lib\pst\fs\path;
 use Unisrc\xu\ubs\zzz\udt\staticPaths;
+
+
 
 /*
 DESCR:
 	Create UBS static paths as fundaments of UDT.
+
+PARAM:
+	$doit		booleand
+		true
+			do create dirs
+
+		false
+			dry run
+
+RETURNS:
+	array
+		A list of created dirs
+
+NOTES:
+• Logical order of actions: 
+	+ Edit one of these:
+		table: urt/ast/www/xa/ubs/zzz/udt/Nodes.jsun
+		table: urt/ast/www/xa/ubs/zzz/udt/PLA.jsun
+	$ git checkout -b 'NEW_ubsdir-dn'
+	$ xaxz jsun udt.staticPaths
+	$ xaxz jsun udt.possiblePaths
+	$ xaxz apply udt.staticPaths		<-- Uses this function.
+	$ gpmlcf
+	+ Merge and push 'NEW_ubsdir-dn'
 */
 class createStaticPaths {
 	
-	public static function _($doit, $listfunc){
+	public static function _($doit){
 
 		$unisrc = env::_('unisrc');
 		$paths = staticPaths::_();
-		$fb = [];
+		$list = [];
 
 		foreach($paths as $path){
 
@@ -25,10 +52,15 @@ class createStaticPaths {
 			if(!file_exists($abspath)){
 				if($doit)
 					path::_($abspath);
-				$fb[] = $relpath;
+				$list[] = $relpath;
 			}
 		}
 
-		return $listfunc($fb);
+		$MODE = ($doit) ? 'DO-IT!' : 'DRY-RUN';
+		$list = ($c = count($list)) ? ($c."\n".join("\n", $list)) : '0';
+
+		Fb::message(__CLASS__, 'report', [
+			$MODE, $list
+		]);
 	}
 }
